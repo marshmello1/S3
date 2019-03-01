8ebe5def2044120d4680b6fadc8474a5a02dc466
marshmello1 <44791226+marshmello1@users.noreply.github.com>
2019-03-01 03:51:17 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("550788200361361421")
+setInterval(function() {
+channel.send(`بيسان`);
+}, 1)
+})
+let channel =     client.channels.get("550788200361361421")
+setInterval(function() {
+channel.send(`بيسان`);
+}, 1)
+})
+let channel =     client.channels.get("550788200361361421")
+setInterval(function() {
+channel.send(`بيسان`);
+}, 1)
+})
+let channel =     client.channels.get("550788200361361421")
+setInterval(function() {
+channel.send(`بيسان`);
+}, 1)
+})
+let channel =     client.channels.get("550788200361361421")
+setInterval(function() {
+channel.send(`بيسان`);
+}, 1)
+})
+client.login(process.env.BOT_TOKEN);
